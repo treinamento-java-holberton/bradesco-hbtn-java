daf2b97488f4f0e2824ca3c4960c3e5d59b9c3f8
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-07-11 10:19:00 -0300

    update

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,4 +1,20 @@
 public class PersonagemGame {
     public int saudeAtual;
     public String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
