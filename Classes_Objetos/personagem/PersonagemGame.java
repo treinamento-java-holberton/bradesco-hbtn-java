7fd2936ccc9d5429701bd4ad786de971385124cb
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-07-11 10:12:36 -0300

    update

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,5 +1,3 @@
-package Classes_Objetos.personagem;
-
 public class PersonagemGame {
     public int saudeAtual;
     public String nome;
--- Classes_Objetos/personagem/Program.java
+++ Classes_Objetos/personagem/Program.java
@@ -1,5 +1,3 @@
-package Classes_Objetos.personagem;
-
 public class Program {
     public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame();
