2276be3879dff0375085254ac1506819b4e9a48b
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-07-12 15:39:39 -0300

    update

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -4,15 +4,23 @@ public class Gerente extends Empregado {
         super(salarioFixo);
     }
 
+
+
     @Override
     public double calcularBonus(Departamento departamento) {
+        double bonusBase = (departamento.alcancouMeta()) ? getSalarioFixo() * 0.2 : 0;
+
+        return bonusBase + calcularBonusExtra(departamento);
+    }
+
+
+    public double calcularBonusExtra(Departamento departamento) {
         double coeficienteBonus = 0.01;
-        double bonusExtra = 0;
+        double bonusExtra = 0.0;
         if (departamento.alcancouMeta()) {
             double superavitDaMeta =  (departamento.getValorAtingidoMeta() - departamento.getValorMeta());
             bonusExtra = coeficienteBonus * superavitDaMeta;
         }
-
         return bonusExtra;
     }
 }
