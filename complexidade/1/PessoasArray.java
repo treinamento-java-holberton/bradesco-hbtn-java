88c57a347dde6baf0f2151b20a0ebd6bdeef7f72
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-07-30 19:12:19 -0300

    1. complexidade O(n)

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -22,7 +22,7 @@ public class PessoasArray {
     // implementar o método de buscaLinear
     public void buscaLinear(String nome){
         boolean encontrado = false;
-        System.out.println("Procurando pelo nome \"%s\"".formatted(nome));
+        System.out.println("Procurando pelo nome: \"%s\"".formatted(nome));
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:%d".formatted(i));
             if (nomes[i] == nome) {
