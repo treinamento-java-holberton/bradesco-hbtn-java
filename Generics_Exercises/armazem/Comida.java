52d2006804d234bb58d333ac895c09f3ebad6e8b
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-08-04 12:26:26 -0300

    0. Armazem de items

--- Generics_Exercises/armazem/Comida.java
+++ Generics_Exercises/armazem/Comida.java
@@ -1,11 +1,11 @@
 public class Comida {
 
     private String nome;
-    private int calorias;
+    private double calorias;
     private double preco;
 
 
-    public Comida(String nome, int calorias, double preco) {
+    public Comida(String nome, double calorias, double preco) {
         this.calorias = calorias;
         this.nome = nome;
         this.preco = preco;
@@ -13,6 +13,6 @@ public class Comida {
 
     @Override
     public String toString() {
-        return "[%s] %d R$ %f".formatted(nome, calorias, preco);
+        return "[%s] %f R$ %f".formatted(nome, calorias, preco);
     }
 }
