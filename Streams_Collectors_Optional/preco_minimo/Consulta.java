0004108d76b7eeb523b8983d8ef92d76c4bb182b
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-08-16 21:55:38 -0300

    2. Relatorio Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream().filter(i -> i.getCategoria().equals(CategoriaProduto.LIVRO)).toList();
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        return produtos.stream().filter(i -> i.getPreco() >= precoMinimo).toList();
+    }
+}
