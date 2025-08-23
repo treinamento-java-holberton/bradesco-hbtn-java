0041ab12fb48aeb3dc41fc910f049a9446a02904
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-08-23 11:11:50 -0300

    2. Contagem de pessoas por idade e cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        //chave: TreeMap de TreeSet, sendo chave cargo e valores um treeset das pessoas.
+
+        return pessoas.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)
+                )
+        );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        //chave: nome do cargo, valor: quantidade de pessoas;
+
+        return pessoas.stream().collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                Collectors.counting()));
+    }
+
+    public static Map<String,Map<Integer,Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        //chave: cargo, valor: Map de: idade, quantidade;
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade,Collectors.counting())));
+    }
+}
