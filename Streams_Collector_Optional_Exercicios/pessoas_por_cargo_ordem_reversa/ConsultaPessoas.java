ac1ce103663bb956c4d5953e59806ec5477829ab
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-08-23 10:23:32 -0300

    0. Pessoas agrupadas por cargos em order reversa

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        //chave: TreeMap de TreeSet, sendo chave cargo e valores um treeset das pessoas.
+
+        return pessoas.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)
+                )
+        );
+    }
+}
